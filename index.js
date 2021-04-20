f6852483ee90038f704004176ca57443df40ff87
hiteshkumawatpro <77722116+hiteshkumawatpro@users.noreply.github.com>
2021-04-20 20:55:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+
+var clock = setInterval(clocktiming, 1000);
+function clocktiming(){
+    var d = new Date();
+    var day = d.getUTCDate();
+    var month = d.getUTCMonth()+1;
+    var year = d.getUTCFullYear();
+    var time = d.toLocaleTimeString();
+    document.getElementById('time').innerHTML = time ;
+    document.getElementById('day').innerHTML = day+"-"+month+"-"+year ;
+}
+
+function fn(){
+    var lengthofphone = document.getElementById('numberbox').value;
+    if(lengthofphone.length <= 9 || lengthofphone.length >= 11){
+      document.getElementById('numberalert').innerHTML = " * Enter 10 digits !";
+      document.getElementById('numberalert').setAttribute("style","color: aqua; font-weight: bold; font-size:17px;");
+      document.getElementById('submitbutton').disabled = true;
+      document.getElementById('submitbutton').setAttribute("style"," opacity: .4;cursor:not-allowed;");
+    }
+    else{
+        document.getElementById('numberalert').innerHTML = "";
+ 
+        document.getElementById('submitbutton').disabled = false;
+        document.getElementById('submitbutton').setAttribute("style"," opacity: 1;cursor:pointer;");
+    }
+
+}
